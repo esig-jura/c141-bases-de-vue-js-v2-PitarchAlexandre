ed403777730d046dfb40d8278a1a17df2d92f3ad
Pitarch Alexandre <alexandre.pitarch@divcom.ch>
2025-02-12 08:43:31 +0100

    ex 1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container max-width="700">
     <!-- Donnée de l'exercice -->
-    <exercice-objectifs number="1" />
+    <exercice1-donnee />
     <!-- Zone de travail pour l'exercice -->
     <div class="exe-zone">
       <h2>Zone d'exercice</h2>
@@ -9,17 +9,18 @@
       <!-- Carte statique à transformer -->
       <v-card class="mx-auto my-6 pa-2" max-width="400">
         <!-- Remplacez *** TITRE *** par un contenu dynamique -->
-        <v-card-title>*** TITRE ***</v-card-title>
+        <v-card-title> {{ title }}</v-card-title>
 
         <!-- Remplacez *** DESCRIPTION *** par un contenu dynamique avec une liaison de style -->
-        <v-card-subtitle style="color: black;">
-          *** DESCRIPTION ***
+        <v-card-subtitle :style="{ color:  textColor }">
+          {{ description }}
         </v-card-subtitle>
 
         <!-- Rendez ce bouton fonctionnel pour changer la couleur -->
         <v-card-actions>
-          <v-btn color="primary">
-            Mettre en surbrillance
+          <v-btn color="primary" @click="changeColor">
+            <p v-if="isHighlighted">Réinitialiser la couleur</p>
+            <p v-else>Mettre en surbrillance</p>
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -29,11 +30,27 @@
 
 <script setup>
 // Importation du composant contenant la donnée de l'exercice
-import ExerciceObjectifs from "@/components/ExerciceObjectifs.vue";
+import Exercice1Donnee from "@/components/donnees/Exercice1Donnee.vue";
+import { ref } from 'vue'
+import { sassFalse } from "sass";
 // Données préparées pour l'exercice
 // SUGGESTION : Pensez à rendre ces données réactives pour qu'elles puissent être mises à jour dynamiquement.
 
 const title = "Bienvenue à l'exercice 1";
 const description = "Cet exercice teste le rendu déclaratif et les liaisons d'attributs.";
-const isHighlighted = false;
+const isHighlighted = ref(false);
+const textColor = ref('black');
+
+
+// Change la valeur de isHiglithed apèrs le click
+const changeColor = () => {
+  isHighlighted.value = !isHighlighted.value
+  textColor.value = isHighlighted.value ? 'blue' : 'black';
+  console.log(isHighlighted.value)
+  console.log(textColor.value)
+}
 </script>
+
+<style scoped lang="sass">
+
+</style>
